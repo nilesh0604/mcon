5467021a991d00bdf8a8e3271de6d7dddad30d27
Nilesh Shinde <nilesh.0604@gmail.com>
2017-03-12 19:01:03 +0530

    Functional Requirements Implemented using Angular UI

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+var app = angular.module('mcon', ['ui.bootstrap'])
+    .controller('MainCtrl', function($scope) {
+        $scope.employees = [{
+            'id': 1,
+            'name': 'Nilesh Shinde',
+            'designation': 'Architect',
+            'level': '4',
+            'manager': 'Makarand Natu',
+            'dob': '06-05-1985'
+        }, {
+            'id': 2,
+            'name': 'Thrinadh Kumar',
+            'designation': 'DevOps',
+            'level': '3',
+            'manager': 'Makarand Natu',
+            'dob': '03-06-1983'
+        }, {
+            'id': 3,
+            'name': 'Sunil Mujnani',
+            'designation': 'Developer',
+            'level': '2',
+            'manager': 'Makarand Natu',
+            'dob': '08-02-1982'
+        }];
+
+        $scope.showDetails = function(e){
+        	alert("Employee Name : " + e.employee.name)
+        };
+    })
